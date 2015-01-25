77918226ee9016514f12418a9fc2cf63e3824543
mohan.kumar <mohan.kumar@inmobi.com>
2015-01-25 12:10:13 +0530

    lesson_1

---
+++ app.js
@@ -0,0 +1,23 @@
+var application_root = __dirname,
+	express = require("express"),
+	path = require("path"),
+	mongoose = require('mongoose');
+
+var app = express();
+mongoose.connect('mongodb://localhost/db');
+app.configure(function () {  // app.configure  is deprocated in 4 *
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(application_root, "public")));
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.get('/api', function (req, res) {
+	res.send('Ecomm API is running');
+	console.log(application_root +"hello");
+});
+
+// Launch server
+
+app.listen(3000);
